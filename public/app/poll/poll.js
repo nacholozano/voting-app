c83338a625dc4815f9233f5e67e92033801ad265
nacholozano <nacholozanogui@gmail.com>
2015-12-14 13:12:30 +0000

    Verify delete poll

--- public/app/poll/poll.js
+++ public/app/poll/poll.js
@@ -10,9 +10,15 @@
 		$scope.poll = pollPromise.data;
 
 		// Delete poll
+
 		$scope.deletePoll = function () {
-			polls.deletePoll($scope.poll._id);
-			$state.go('home');
+			polls.deletePoll($scope.poll._id).success(function (message) {
+				//$state.go('home');
+				$scope.poll = '';
+				$scope.message = message.message;
+			}).error(function (error) {
+				$scope.error = error;
+			});
 		};
 
 		// Vote
---
+++ public/directives/uno.js
@@ -0,0 +1,10 @@
+;
+(function () {
+	angular.module('')
+		.directive('', function () {
+			return {
+				restrict: 'E',
+				template:
+			}
+		});
+})();
